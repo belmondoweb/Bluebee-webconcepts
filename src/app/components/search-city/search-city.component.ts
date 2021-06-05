1393230fa814c37fcaf09c2150f0a4b92a04e8c1
Natalie Peretz <belmondoweb@gmail.com>
2021-06-05 19:02:10 +0300

    added dynamic formArray
    Added FormArray

--- src/app/components/search-city/search-city.component.ts
+++ src/app/components/search-city/search-city.component.ts
@@ -1,6 +1,6 @@
 import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormArrayName, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';   
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
 import { City } from 'src/app/interface/ICity';
 
@@ -17,7 +17,7 @@ import { __values } from 'tslib';
   styleUrls: ['./search-city.component.css']
 })
 export class SearchCityComponent implements OnInit {
-  @Input('weatherData') weatherItem: ICity;
+  // @Input('weatherData') weatherItem: ICity;
   public cities$: Observable<ICity[]>; 
   public city: City;
   weatherIconURL = 'https://openweathermap.org/img/w/';
@@ -27,21 +27,23 @@ export class SearchCityComponent implements OnInit {
 private citySub = new Subject<string>();
   form: FormGroup;
   search: FormControl;
-  formArray: FormArray
+  formArray: FormArray;
   cityID: number =6;
   searchText: string|null=null;
   private citySub$: Subject<void> = new Subject(); //maybe will use later if ...
   weatherData: any=[];
-  submitted = false;
+  submitted = false;  
+  weatherIndex: any;
 
   constructor(private _service: WeatherServiceService, private fb: FormBuilder ) { 
-    this.weatherData;
-    
+    this.weatherData; 
   }
 
 
   ngOnInit() {
-  
+    this.form = this.fb.group({   
+      weather : this.fb.array([this.addWeatherGroup()])
+    });
     this.getCityList();
     // this.formArray = new FormArray([
     //   new FormGroup({
@@ -50,22 +52,29 @@ private citySub = new Subject<string>();
     //     temp: new FormControl(''),
     //   })
         
-    // ])
-    this.form = this.fb.group({
-      name:['', Validators.required],
-      temp: ['',Validators.required], 
-    })
-  
+    // ])  
+  }
+
+  addWeatherGroup(){
+    return  this.fb.group({   
+      name:['',Validators.required],
+      temp: ['',Validators.required],
+    });
+  }
+  // name:['',Validators.required],
+  // temp: ['',Validators.required],
+  get weatherArray(){
+  return this.form.get('weather') as FormArray;
   }
 
 // ******** Save to List *********** //
-    onSave(form){
-    //  this.formArray.push(this.form.value)
+    onSave(form,i){
+      this.weatherIndex = i;
+    //  this.formArray.push(this.form.value) 
       this._service.addWeatherItem(this.form.value) 
       localStorage.setItem('local saved cities', JSON.stringify(form));
         // window.alert('Still WORKING ON IT!! lol ;)\n\n' + JSON.stringify(this.form.value, null, 4));
-        console.log('saved?')
-       
+        console.log('saved?') 
     }
 
     // set
@@ -76,17 +85,21 @@ private citySub = new Subject<string>();
 
     // ********** Get data from api **********//
    
-      sendToAPI(formValues){
-      debugger;
+      sendToAPI(formValues){ 
       this._service
-      .fetchrApi(formValues.name, formValues.temp)
+      .fetchrApi( formValues.weather[this.weatherIndex].name, formValues.weather[this.weatherIndex].temp)
       .subscribe(data => {
-        this.weatherData.push(data);
+        this.weatherData.push(data); 
+        this.weatherArray.push(this.addWeatherGroup());
         console.log("Hey are you there? Form value ",formValues?.temp); 
-       
       })
-    }
+      }
     
-  
+  // yesturday ... will not use
+  // searchCity(cityId: string, units){
+  //  this._service.fetchrApi(cityId, units).subscribe(data=>
+  //   this.weatherData = data)
+  //   console.log('Haloha I am here : >',cityId)
+  // }
  
 }
