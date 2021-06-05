19cf04e87c4527e7e367ca5c2e5490dd91591ebc
Natalie Peretz <belmondoweb@gmail.com>
2021-06-05 20:57:19 +0300

    added forecast
    sort by city option

--- src/app/components/forecast-result/forecast-result.component.ts
+++ src/app/components/forecast-result/forecast-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 // import { WeatherService } from 'src/app/forecast.service';
 import { City } from 'src/app/interface/ICity';
 import { WeatherServiceService } from 'src/app/services/weather-service.service';
@@ -9,16 +11,67 @@ import { WeatherServiceService } from 'src/app/services/weather-service.service'
   styleUrls: ['./forecast-result.component.scss']
 })
 export class ForecastResultComponent implements OnInit {
-  @Input('item') weatherItem: City;
-  @Output() weatherData;
-  constructor( private weatherService: WeatherServiceService) { }
+  // @Input('item') weatherItem: City;
+  // @Output() weatherData;
+  public city: City;
+  weatherIconURL = 'https://openweathermap.org/img/w/';
+  public citiesWeather: City[] = [];
+  form: FormGroup;
+  search: FormControl;
+  formArray: FormArray;
+  weatherData: any=[];
+  submitted = false;  
+  weatherIndex: any;
 
-  public current: City;
+  constructor( private _service: WeatherServiceService, private fb: FormBuilder) { }
+public current: City;
   
   ngOnInit(): void {
-
+    this.form = this.fb.group({   
+      weather : this.fb.array([this.addWeatherGroup()])
+    });
+    this.getCityList();
   }
 
+
+  addWeatherGroup(){
+    return  this.fb.group({   
+      name:['',Validators.required],
+      temp: ['',Validators.required],
+    });
+  } 
+  
+  get weatherArray(){
+  return this.form.get('weather') as FormArray;
+  }
+
+// ******** Save to List *********** //
+    onSave(form,i){
+      this.weatherIndex = i;
+    //  this.formArray.push(this.form.value) 
+      this._service.addWeatherItem(this.form.value) 
+      localStorage.setItem('local saved cities', JSON.stringify(form));
+        // window.alert('Still WORKING ON IT!! lol ;)\n\n' + JSON.stringify(this.form.value, null, 4));
+        console.log('saved?') 
+    }
+
+    // set
+
+    getCityList(){
+      this._service.getWeatherItems()
+    }
+
+    // ********** Get data from api **********//
+   
+      sendToAPI(formValues){ 
+      this._service
+      .fetchrApi( formValues.weather[this.weatherIndex].name, formValues.weather[this.weatherIndex].temp)
+      .subscribe(data => {
+        this.weatherData.push(data); 
+        this.weatherArray.push(this.addWeatherGroup());
+        console.log("Hey are you there? Form value ",formValues?.temp); 
+      })
+      } 
 
 
 }
